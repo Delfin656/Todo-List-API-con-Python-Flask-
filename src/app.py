c9a6ff991513585e047979836aeeec129f015ad6
Delfin656 <juandelfin656@gmail.com>
2022-11-27 07:00:40 +0000

    Endpoints con tres metodos GET POST y DELETE

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify,request, json
+app = Flask(__name__)
+todos = [
+    {"label": "My first task", "done": False},
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decodificado = json.loads(request.data)
+    todos.append(decodificado)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE']) 
+def delete_todo(position):
+    delete_todo = request.data
+    todos.pop()
+    print("This is the position to delete", position)
+    return jsonify(todos)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
+
